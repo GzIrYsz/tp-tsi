4f9a52ba7aa7df5c893cd6f1fd39940bc3c41afa
Thomas REMY <contact@thomasremy.fr>
2025-01-17 18:46:35 +0100

    fix: Refactor edge detection algorithm and add threshold constant
    The edge detection algorithm in the grayscale image processing module has been refactored for improved readability and maintainability. The changes include:
    - Introduced a new macro EDGE_THRESHOLD to replace hardcoded values, enhancing flexibility.
    - Adjusted the calculation of horizontal_sum and vertical_sum for better clarity.
    - Modified the output filename to include the threshold value, aiding in result differentiation when varying thresholds are used.

--- src/tp1/edge_detection_grayscale.c
+++ src/tp1/edge_detection_grayscale.c
@@ -7,6 +7,11 @@
 #include "nrarith.h"
 #include "nrio.h"
 
+#define STR_(X) #X
+#define STR(X) STR_(X)
+
+#define EDGE_THRESHOLD 15
+
 int main(int argc, char **argv) {
     long nrl, nrh, ncl, nch;
     byte **input_img = LoadPGM_bmatrix("data/input/cubes.pgm", &nrl, &nrh, &ncl, &nch);
@@ -21,24 +26,24 @@ int main(int argc, char **argv) {
     for (int i = nrl + 1; i < nrh; i++) {
         for (int j = ncl + 1; j < nch; j++) {
             horizontal_sum = input_img[i - 1][j - 1] * -1
-                            + input_img[i][j - 1] * 0
-                            + input_img[i + 1][j - 1] * 1
-                            + input_img[i - 1][j] * -2
+                            + input_img[i - 1][j] * 0
+                            + input_img[i - 1][j + 1] * 1
+                            + input_img[i][j - 1] * -2
                             + input_img[i][j] * 0
-                            + input_img[i + 1][j] * 2
-                            + input_img[i - 1][j + 1] * -1
-                            + input_img[i][j + 1] * 0
+                            + input_img[i][j + 1] * 2
+                            + input_img[i + 1][j - 1] * -1
+                            + input_img[i + 1][j] * 0
                             + input_img[i + 1][j + 1] * 1;
             horizontal_sum /= 4;
 
             vertical_sum = input_img[i - 1][j - 1] * -1
-                            + input_img[i][j - 1] * -2
-                            + input_img[i + 1][j - 1] * -1
-                            + input_img[i - 1][j] * 0
+                            + input_img[i - 1][j] * -2
+                            + input_img[i - 1][j + 1] * -1
+                            + input_img[i][j - 1] * 0
                             + input_img[i][j] * 0
-                            + input_img[i + 1][j] * 0
-                            + input_img[i - 1][j + 1] * 1
-                            + input_img[i][j + 1] * 2
+                            + input_img[i][j + 1] * 0
+                            + input_img[i + 1][j - 1] * 1
+                            + input_img[i + 1][j] * 2
                             + input_img[i + 1][j + 1] * 1;
             vertical_sum /= 4;
 
@@ -47,13 +52,13 @@ int main(int argc, char **argv) {
 
             gradient_norm = (int) sqrt(pow(horizontal_sum, 2) + pow(vertical_sum, 2));
             output_img_horizontal_vertical_gradient[i][j] = gradient_norm;
-            output_img_edges_detection[i][j] = gradient_norm > 15 ? 255 : 0;
+            output_img_edges_detection[i][j] = gradient_norm > EDGE_THRESHOLD ? 255 : 0;
         }
     }
     SavePGM_bmatrix(output_img_horizontal_gradient, nrl, nrh, ncl, nch, "data/output/cubes_horizontal_gradient.pgm");
     SavePGM_bmatrix(output_img_vertical_gradient, nrl, nrh, ncl, nch, "data/output/cubes_vertical_gradient.pgm");
     SavePGM_bmatrix(output_img_horizontal_vertical_gradient, nrl, nrh, ncl, nch, "data/output/cubes_horizontal_vertical_gradient.pgm");
-    SavePGM_bmatrix(output_img_edges_detection, nrl, nrh, ncl, nch, "data/output/cubes_edges_detection.pgm");
+    SavePGM_bmatrix(output_img_edges_detection, nrl, nrh, ncl, nch, "data/output/cubes_edges_detection_t" STR(EDGE_THRESHOLD) ".pgm");
 
     free_bmatrix(input_img, nrl, nrh, ncl, nch);
     free_bmatrix(output_img_horizontal_gradient, nrl, nrh, ncl, nch);
